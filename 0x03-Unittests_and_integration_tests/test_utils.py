35cc50e53934af82e4a2ea81918f6fa7884faa26
mr-roko <rbbagyei@gmail.com>
2024-12-15 21:18:58 +0000

    file refactored to follow pycodestyle

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""Module for testing utility functions."""
 
 from unittest import TestCase
 from unittest.mock import patch, Mock
@@ -8,33 +9,26 @@ import unittest
 utils = __import__("utils")
 memoize = utils.memoize
 
+
 class TestAccessNestedMap(TestCase):
-    """
-    Tests for utils.access_nested_map function
-    @parameterized.expand() decorator used to test multiple inputs
- 
-    """
+    """Tests for utils.access_nested_map function."""
+
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map, path, expected_value):
-        """
-            A unit test for the utils.access_nested_map function.
-            Purpose
-            -------
-            To check for correct return values
+        """Test access_nested_map returns correct values.
 
-            Parameters
-            ----------
-            nested_map: Mapping
-                A nested map
-            path: Sequence
-                a sequence of key representing a path to the value
-            expected_value:
-                a value to check result of function against
-
+        Parameters
+        ----------
+        nested_map : Mapping
+            A nested map
+        path : Sequence
+            A sequence of keys representing a path to the value
+        expected_value : Any
+            Expected return value from the function
         """
         result = utils.access_nested_map(nested_map, path)
         self.assertEqual(result, expected_value)
@@ -43,22 +37,18 @@ class TestAccessNestedMap(TestCase):
         ({}, ('a',), "'a'"),
         ({"a": 1}, ("a", "b"), "'b'")
     ])
-    def test_access_nested_map_exception(self, nested_map, path, expected_error_message):
-        """
-            A unit test for the utils.access_nested_map function.
-            Purpose
-            -------
-            To check for exceptions
+    def test_access_nested_map_exception(self, nested_map, path,
+                                       expected_error_message):
+        """Test access_nested_map raises correct exceptions.
 
-            Parameters
-            ----------
-            nested_map: Mapping
-                A nested map
-            path: Sequence
-                a sequence of key representing a path to the value
-            expected_error_message:
-                Error message to receive after KeyError exception
-
+        Parameters
+        ----------
+        nested_map : Mapping
+            A nested map
+        path : Sequence
+            A sequence of keys representing a path to the value
+        expected_error_message : str
+            Expected error message from KeyError exception
         """
         with self.assertRaises(KeyError) as map_context:
             utils.access_nested_map(nested_map, path)
@@ -67,30 +57,24 @@ class TestAccessNestedMap(TestCase):
 
 
 class TestGetJson(TestCase):
-    """
-    Testcase for testing the utils.get_json function
-    """
+    """Tests for utils.get_json function."""
+
     @parameterized.expand([
-            ("http://example.com", {"payload": True}),
-            ("http://holberton.io", {"payload": False})
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
     ])
     @patch('requests.get')
     def test_get_json(self, test_url, test_payload, mock_request_get):
-        """
-            A unit test for the utils.get_json function.
-            Purpose
-            -------
-            To mock request.get and make sure it is called as well as return a json object
+        """Test get_json returns correct JSON payload.
 
-            Parameters
-            ----------
-            test_url:
-                Url to mock request to
-            test_payload:
-                a json object expected to be returned
-            mock_request_get:
-                mock object from the fake patched request.get
-
+        Parameters
+        ----------
+        test_url : str
+            URL to mock request to
+        test_payload : dict
+            Expected JSON response
+        mock_request_get : Mock
+            Mock object for patched request.get
         """
         new_mock = Mock()
         new_mock.json.return_value = test_payload
@@ -101,17 +85,15 @@ class TestGetJson(TestCase):
         mock_request_get.assert_called_once_with(test_url)
         self.assertEqual(result, test_payload)
 
-class TestMemoize(unittest.TestCase):
-    """
-    Test case to test the memoize decorator
-    """
-    def test_memoize(self):
-        """
-            A unit test for the utils.memoize decorator function.
-            Purpose
-            -------
-            To mock a_method and make sure it is called only once since memoization is being implemented through the decorator
 
+class TestMemoize(TestCase):
+    """Tests for utils.memoize decorator."""
+
+    def test_memoize(self):
+        """Test memoize decorator caches method results correctly.
+
+        Verifies that the decorated method is called only once
+        and its result is properly cached.
         """
         class TestClass:
             def a_method(self):
@@ -120,12 +102,12 @@ class TestMemoize(unittest.TestCase):
             @memoize
             def a_property(self):
                 return self.a_method()
-            
+
         test_object = TestClass()
-        
+
         with patch.object(TestClass, 'a_method') as a_method_mock:
             a_method_mock.return_value = 42
-            
+
             response1 = test_object.a_property
             response2 = test_object.a_property
 
