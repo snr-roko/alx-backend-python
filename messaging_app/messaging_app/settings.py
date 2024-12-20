d322413dad1eae8c5acfcca994745b05210a4110
mr-roko <rbbagyei@gmail.com>
2024-12-20 18:22:52 +0000

    Add Django REST framework settings for authentication and permissions

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -125,3 +125,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# djangorestframework settings
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": ['rest_framework.permissions.isAuthenticated'],
+    "DEFAULT_AUTHENTICATION_CLASSES": ['rest_framework.authentication.SessionAuthentication']
+}
