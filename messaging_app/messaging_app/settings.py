19b396845625cc8a6cd8ee05c08c7ca4b4e61aa4
mr-roko <rbbagyei@gmail.com>
2024-12-20 18:25:37 +0000

    Fix formatting of REST framework settings for consistency

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -128,6 +128,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # djangorestframework settings
 REST_FRAMEWORK = {
-    "DEFAULT_PERMISSION_CLASSES": ['rest_framework.permissions.isAuthenticated'],
-    "DEFAULT_AUTHENTICATION_CLASSES": ['rest_framework.authentication.SessionAuthentication']
+    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated"],
+    "DEFAULT_AUTHENTICATION_CLASSES": ["rest_framework.authentication.SessionAuthentication"]
 }
