1ed36e097bfbd01270cb59fd5731b3c22948a638
mr-roko <rbbagyei@gmail.com>
2025-01-12 19:31:46 +0000

    Add MessageHistory model and implement pre_save signal for message edits

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -8,9 +8,18 @@ class Message(models.Model):
     receiver = models.ForeignKey(User, related_name='messages_received', on_delete=models.PROTECT)
     timestamp = models.DateTimeField(auto_now_add=True)
     content = models.TextField()
+    edited = models.BooleanField(default=False)
 
 class Notification(models.Model):
     notification_id = models.UUIDField(primary_key=True, default=uuid4, db_index=True)
     message = models.ForeignKey(Message, related_name='notifications', on_delete=models.CASCADE)
     read = models.BooleanField(default=False)
     timestamp = models.DateTimeField(auto_now_add=True)
+
+class MessageHistory(models.Model):
+    history_id = models.UUIDField(primary_key=True, default=uuid4, db_index=True)
+    message_id = models.UUIDField(blank=False, null=False)
+    content = models.TextField()
+    edited_at = models.DateTimeField(auto_now_add=True)
+    edited_by = models.ForeignKey(User, related_name='message_edited', on_delete=models.PROTECT)
+
--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,8 +1,16 @@
 from django.dispatch import receiver
-from django.db.models.signals import post_save, post_delete
-from .models import Message, Notification
+from django.db.models.signals import post_save, post_delete, pre_save
+from .models import Message, Notification, MessageHistory   
 
 @receiver(post_save, sender=Message)
 def create_notification(sender, instance, created, **kwargs):
     if created:
         Notification.objects.create(message=instance)
+
+@receiver(pre_save, sender=Message)
+def add_message_history(sender, instance, created, **kwargs):
+    MessageHistory.objects.create(
+        message_id = instance.message_id,
+        content = instance.content,
+        edited_by = instance.sender
+    )
